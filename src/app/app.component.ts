c96124900b077fbbca8b9bca8370400f6eee0d3e
Amanda Grice <algrice@bu.edu>
2019-10-05 20:58:15 -0400

    add form field for card name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   frameResource: string = "../assets/black_card_frame.png";
+  cardName: string;
 
   swapFrameColor(colorSrc) {
     this.frameResource = colorSrc;
+  }
+
+  nameCard(event) {
+    console.log(event);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {ButtonModule} from "primeng/button";
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    InputTextModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
